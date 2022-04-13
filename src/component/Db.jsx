d8a1e52423c0e2947168ebb00334ab0197dc4157
huynguyenvan27 <89083694+huynguyenvan27@users.noreply.github.com>
2022-04-13 07:41:31 +0700

    update

--- src/component/Db.jsx
+++ src/component/Db.jsx
@@ -1,40 +1,44 @@
-
+import song1 from '/mp3/CuoiThoi-MasewMasiuBRayTAPVietNam-7085648.mp3'
+import song2 from '/mp3/HayTraoChoAnh-SonTungMTPSnoopDogg-6010660.mp3'
+import song3 from '/mp3/MuonRoiMaSaoCon-SonTungMTP-7011803.mp3'
+import song4 from '/mp3/ThangMayEmNhoAnh-HaAnhTuan-6995531.mp3'
+import song5 from '/mp3/ThucGiac-DaLAB-7048212.mp3'
 
 const Data = [
   {
     id:0,
     title  : 'Cưới thôi',
     singer : 'Bray',
-    image : 'public/img/photo-1-16447525869191476343003.webp',
-    path : 'public/mp3/CuoiThoi-MasewMasiuBRayTAPVietNam-7085648.mp3'
+    image : 'https://media.thieunien.vn/upload/hachi/2022/02/14/tuong-b-ray-len-xe-hoa-den-noi-hoa-ra-tat-ca-chi-la-thuyet-am-muu-1644825399-1.jpeg',
+    path : song1
   },
   {
     id:1,
     title  : 'Hãy trao cho anh',
     singer : 'Sơn Tùng-MTP',
     image : 'https://image-us.24h.com.vn/upload/3-2019/images/2019-07-02/hay-trao-cho-anh-son-tung-1562044993-752-width660height525.jpg',
-    path : 'public/mp3/HayTraoChoAnh-SonTungMTPSnoopDogg-6010660.mp3'
+    path : song2
   },
   {
     id:2,
     title  : 'Muộn rồi mà sao còn',
     singer : 'Sơn Tùng - MTP',
     image : 'https://vtv1.mediacdn.vn/thumb_w/650/2022/1/7/muon-roi-ma-sao-con-16415172126631362332320.jpg',
-    path : 'public/mp3/MuonRoiMaSaoCon-SonTungMTP-7011803.mp3'
+    path : song3
   },
   {
     id:3,
     title  : 'Tháng mấy em nhớ anh',
     singer : 'Bùi Anh Tuấn',
     image : 'https://i.scdn.co/image/ab67616d0000b273d12312aa0e25b7ddb9eadbd9',
-    path : 'public/mp3/ThangMayEmNhoAnh-HaAnhTuan-6995531.mp3'
+    path : song4
   },
   {
     id:4,
     title  : 'Thức giấc',
     singer : 'DaLAB',
     image : 'https://reviewsmoi.com/wp-content/uploads/2021/07/thuc-giac-img.jpg',
-    path : 'public/mp3/ThucGiac-DaLAB-7048212.mp3',
+    path : song5
   }
 ]
 
